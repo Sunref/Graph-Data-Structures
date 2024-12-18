87cf0eb557c5cbefb88a9ae7782348e9463afa94
RezeScarlet <gui171204@gmail.com>
2024-12-18 14:38:30 -0300

    Trying to fix shit

--- src/grafo/Aresta.java
+++ src/grafo/Aresta.java
@@ -21,4 +21,22 @@ public class Aresta {
         e.drawLine( origem.pos, destino.pos, EngineFrame.BLACK );
     }
     
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Aresta aresta = (Aresta) obj;
+        return (origem.equals(aresta.origem) && destino.equals(aresta.destino))
+                || (origem.equals(aresta.destino) && destino.equals(aresta.origem));
+    }
+
+    @Override
+    public int hashCode() {
+        return origem.hashCode() + destino.hashCode();
+    }
+    
 }
--- src/grafo/Grafo.java
+++ src/grafo/Grafo.java
@@ -13,144 +13,168 @@ import java.util.TreeMap;
 
 /**
  * Um grafo implementado usando uma tabela de símbolos.
- * 
+ *
  * @author Prof. Dr. David Buzatto
  */
 public class Grafo {
 
-  public Map<Vertice, List<Aresta>> st;
-  public Map<Integer, Vertice> vertices;
+    public Map<Vertice, List<Aresta>> st;
+    public Map<Integer, Vertice> vertices;
 
-  public Grafo() {
-    st = new TreeMap<>();
-    vertices = new TreeMap<>();
-  }
-
-  public Vertice addVertice(double x, double y) {
-    Vertice v = new Vertice(vertices.size(), x, y);
-    vertices.put(v.id, v);
-    return v;
-  }
-
-  public void addAresta(int origem, int destino) {
-    Vertice vo = vertices.get(origem);
-    Vertice vd = vertices.get(destino);
-    if (!st.containsKey(vo)) {
-      st.put(vo, new ArrayList<>());
-    }
-    if (!st.containsKey(vd)) {
-      st.put(vd, new ArrayList<>());
-    }
-    st.get(vo).add(0, new Aresta(vo, vd));
-    st.get(vd).add(0, new Aresta(vd, vo));
-  }
-
-  public List<Aresta> adjacentes(int origem) {
-    return st.getOrDefault(vertices.get(origem), new ArrayList<>());
-  }
-
-  public int getQuantidadeVertices() {
-    return vertices.size();
-  }
-
-  public void draw(EngineFrame e) {
-
-    for (Map.Entry<Vertice, List<Aresta>> entry : st.entrySet()) {
-      for (Aresta a : entry.getValue()) {
-        a.draw(e);
-      }
+    public Grafo() {
+        st = new TreeMap<>();
+        vertices = new TreeMap<>();
     }
 
-    for (Map.Entry<Integer, Vertice> entry : vertices.entrySet()) {
-      entry.getValue().draw(e);
+    public Vertice addVertice(double x, double y) {
+        Vertice v = new Vertice(vertices.size(), x, y);
+        vertices.put(v.id, v);
+        return v;
     }
 
-  }
-
-  @Override
-  public String toString() {
-
-    StringBuilder sb = new StringBuilder();
-
-    for (Map.Entry<Vertice, List<Aresta>> entry : st.entrySet()) {
-      sb.append(entry.getKey()).append(" -> ");
-      boolean primeiro = true;
-      for (Aresta a : entry.getValue()) {
-        if (primeiro) {
-          primeiro = false;
-        } else {
-          sb.append(", ");
+    public void addAresta(int origem, int destino) {
+        Vertice vo = vertices.get(origem);
+        Vertice vd = vertices.get(destino);
+        if (!st.containsKey(vo)) {
+            st.put(vo, new ArrayList<>());
         }
-        sb.append(a.destino.id);
-      }
-      sb.append("\n");
+        if (!st.containsKey(vd)) {
+            st.put(vd, new ArrayList<>());
+        }
+        st.get(vo).add(0, new Aresta(vo, vd));
+        st.get(vd).add(0, new Aresta(vd, vo));
     }
 
-    return sb.toString().trim();
+    public List<Aresta> adjacentes(int origem) {
+        return st.getOrDefault(vertices.get(origem), new ArrayList<>());
+    }
 
-  }
+    public int getQuantidadeVertices() {
+        return vertices.size();
+    }
 
-  // INFO A lista retornada pode ser usada para desenhar as setas da arvore resultante,
-  // pois cada aresta armazena seu destinho e origem
-  public List<Aresta> DFS(Vertice origem) {
+    public void draw(EngineFrame e) {
 
-    Set<Vertice> visitados = new HashSet<>();
-    List<Aresta> edgeList = new ArrayList<>();
-    Stack<Vertice> stack = new Stack<>();
-
-    stack.push(origem);
-
-    while (!stack.isEmpty()) {
-      Vertice atual = stack.pop();
-
-      if (!visitados.contains(atual)) {
-        visitados.add(atual);
-
-        for (Aresta aresta : st.get(atual)) {
-           if (!visitados.contains(aresta.destino)) {
-                if (!edgeList.contains(aresta)) {
-                    edgeList.add(aresta);
-                }
-                
-                if (!stack.contains(aresta.destino)) {
-                    stack.push(aresta.destino);
-                }
-                
+        for (Map.Entry<Vertice, List<Aresta>> entry : st.entrySet()) {
+            for (Aresta a : entry.getValue()) {
+                a.draw(e);
             }
         }
-      }
+
+        for (Map.Entry<Integer, Vertice> entry : vertices.entrySet()) {
+            entry.getValue().draw(e);
+        }
+
     }
 
-    return edgeList;
-  }
+    @Override
+    public String toString() {
 
-  public List<Aresta> BFS(Vertice origem) {
+        StringBuilder sb = new StringBuilder();
 
-    Set<Vertice> visitados = new HashSet<>();
-    List<Aresta> edgeList = new ArrayList<>();
-    Queue<Vertice> fila = new LinkedList<>();
-    fila.add(origem);
-
-    while (!fila.isEmpty()) {
-      Vertice atual = fila.poll();
-
-      if (!visitados.contains(atual)) {
-        visitados.add(atual);
-
-        for (Aresta aresta : st.get(atual)) {
-          if (!visitados.contains(aresta.destino)) {
-                if (!edgeList.contains(aresta)) {
-                    edgeList.add(aresta);
+        for (Map.Entry<Vertice, List<Aresta>> entry : st.entrySet()) {
+            sb.append(entry.getKey()).append(" -> ");
+            boolean primeiro = true;
+            for (Aresta a : entry.getValue()) {
+                if (primeiro) {
+                    primeiro = false;
+                } else {
+                    sb.append(", ");
                 }
-                
-                if (!fila.contains(aresta.destino)) {
-                    fila.add(aresta.destino);
+                sb.append(a.destino.id);
+            }
+            sb.append("\n");
+        }
+
+        return sb.toString().trim();
+
+    }
+
+    // INFO A lista retornada pode ser usada para desenhar as setas da arvore resultante,
+    // pois cada aresta armazena seu destinho e origem
+//    public Set<Aresta> DFS(Vertice origem) {
+//    Set<Vertice> visitados = new HashSet<>();
+//    Set<Aresta> edgeSet = new HashSet<>();
+//    Stack<Vertice> stack = new Stack<>();
+//    stack.push(origem);
+//    visitados.add(origem);
+//
+//    while (!stack.isEmpty()) {
+//        Vertice atual = stack.pop();
+//        System.out.println("Visitando nó: " + atual.id);
+//
+//        List<Aresta> adjacentes = st.get(atual); 
+//        if (adjacentes != null) { 
+//            for (int i = adjacentes.size() - 1; i >= 0; i--) { 
+//                Aresta aresta = adjacentes.get(i); 
+//                Vertice destino = aresta.destino;
+//
+//                if (!visitados.contains(destino)) {
+//                    edgeSet.add(aresta);
+//                    stack.push(destino);
+//                    visitados.add(destino);
+//                }
+//            }
+//        }
+//    }
+//
+//    return edgeSet;
+//}
+    public Set<Aresta> DFS(Vertice origem) {
+        Set<Vertice> visitados = new HashSet<>();
+        Set<Aresta> edgeSet = new HashSet<>();
+        Stack<Vertice> stack = new Stack<>();
+        stack.push(origem);
+        visitados.add(origem);
+
+        while (!stack.isEmpty()) {
+            Vertice atual = stack.pop();
+            System.out.println("Visitando no: " + atual.id);
+
+            List<Aresta> adjacentes = st.get(atual);
+            if (adjacentes != null) {
+                for (int i = adjacentes.size(); i >= 0; i--) {
+                    Aresta aresta = adjacentes.get(i);
+                    Vertice destino = aresta.destino;
+
+                    if (!visitados.contains(destino)) {
+                        edgeSet.add(aresta);
+                        stack.push(destino);
+                        visitados.add(destino);
+                    }
                 }
             }
         }
-      }
+
+        return edgeSet;
     }
 
-    return edgeList;
-  }
+    public Set<Aresta> BFS(Vertice origem) {
+        Set<Vertice> visitados = new HashSet<>();
+        Set<Aresta> edgeSet = new HashSet<>();
+        Queue<Vertice> fila = new LinkedList<>();
+        fila.add(origem);
+        visitados.add(origem);
+
+        while (!fila.isEmpty()) {
+            Vertice atual = fila.poll();
+            System.out.println("Visitando no: " + atual.id);
+
+            List<Aresta> adjacentes = st.get(atual);
+            if (adjacentes != null) {
+                for (Aresta aresta : adjacentes) {
+                    Vertice destino = aresta.destino;
+
+                    if (!visitados.contains(destino)) {
+                        edgeSet.add(aresta);
+                        fila.add(destino);
+                        visitados.add(destino);
+                    }
+                }
+            }
+        }
+
+        return edgeSet;
+    }
+
 }
